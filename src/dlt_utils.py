b5f104c37d47ab91bdece71ecbe1d1167302ec35
MerlinSchaefer <ms101196@googlemail.com>
2024-10-15 15:40:58 +0200

    fix lookup table creation

--- src/dlt_utils.py
+++ src/dlt_utils.py
@@ -1,9 +1,10 @@
 import yaml
-from pyspark.sql import types as T
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import lit
 from databricks.sdk.runtime import *
 
-def load_schema_from_yaml(yaml_path: str, schema_name: str) -> T.StructType:
+def load_schema_from_yaml(yaml_path: str, schema_name: str) -> StructType:
     """
     Load a schema from a YAML file into Spark format.
 
@@ -21,9 +22,9 @@ def load_schema_from_yaml(yaml_path: str, schema_name: str) -> T.StructType:
     schema_fields = schemas[schema_name]
     
     # Convert the schema definition into a Spark StructType
-    fields = [T.StructField(field['name'], getattr(T, field['type'])(), True) for field in schema_fields]
+    fields = [StructField(field['name'], getattr(T, field['type'])(), True) for field in schema_fields]
     
-    return T.StructType(fields)
+    return StructType(fields)
 
 
 def load_yaml_config(config_path:str) -> dict:
@@ -52,4 +53,10 @@ def create_lookup_df(mapping: dict, column_name: str) -> DataFrame:
         A Spark DataFrame with two columns: column and string_value
     """
     lookup_list = [(k, v) for k, v in mapping.items()]
-    return spark.createDataFrame(lookup_list, schema=["string_value", "numeric_value"]).withColumn("column", F.lit(column_name))
+        # Define an explicit schema
+    schema = StructType([
+        StructField("string_value", StringType(), True),
+        StructField("numeric_value", DoubleType(), True),
+        StructField("column", StringType(), True)
+    ])
+    return spark.createDataFrame(lookup_list, schema=schema).withColumn("column", lit(column_name))
