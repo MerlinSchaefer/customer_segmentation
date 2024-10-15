64875caabf3fe1938ebb1f9d8fabf47346682a09
MerlinSchaefer <ms101196@googlemail.com>
2024-10-15 15:43:16 +0200

    fix imports

--- src/dlt_utils.py
+++ src/dlt_utils.py
@@ -1,10 +1,10 @@
 import yaml
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+import pyspark.sql.types as T
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import lit
 from databricks.sdk.runtime import *
 
-def load_schema_from_yaml(yaml_path: str, schema_name: str) -> StructType:
+def load_schema_from_yaml(yaml_path: str, schema_name: str) -> T.StructType:
     """
     Load a schema from a YAML file into Spark format.
 
@@ -13,7 +13,7 @@ def load_schema_from_yaml(yaml_path: str, schema_name: str) -> StructType:
         schema_name (str): Name of the schema to load. 
                             Must match the YAML.
     Returns:
-        A Spark StructType object for the specified schema.
+        A Spark T.StructType object for the specified schema.
     """
     with open(yaml_path, 'r') as yaml_file:
         schemas = yaml.safe_load(yaml_file)
@@ -21,10 +21,10 @@ def load_schema_from_yaml(yaml_path: str, schema_name: str) -> StructType:
     # Extract the specified schema
     schema_fields = schemas[schema_name]
     
-    # Convert the schema definition into a Spark StructType
-    fields = [StructField(field['name'], getattr(T, field['type'])(), True) for field in schema_fields]
+    # Convert the schema definition into a Spark T.StructType
+    fields = [T.StructField(field['name'], getattr(T, field['type'])(), True) for field in schema_fields]
     
-    return StructType(fields)
+    return T.StructType(fields)
 
 
 def load_yaml_config(config_path:str) -> dict:
@@ -54,9 +54,9 @@ def create_lookup_df(mapping: dict, column_name: str) -> DataFrame:
     """
     lookup_list = [(k, v) for k, v in mapping.items()]
         # Define an explicit schema
-    schema = StructType([
-        StructField("string_value", StringType(), True),
-        StructField("numeric_value", DoubleType(), True),
-        StructField("column", StringType(), True)
+    schema = T.StructType([
+        T.StructField("string_value", T.StringType(), True),
+        T.StructField("numeric_value", T.DoubleType(), True),
+        T.StructField("column", T.StringType(), True)
     ])
     return spark.createDataFrame(lookup_list, schema=schema).withColumn("column", lit(column_name))
