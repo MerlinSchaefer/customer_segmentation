eaeec1bb500f2252b5d48ff166bfb1bb1608fd26
MerlinSchaefer <MerlinSchaefer>
2024-10-17 07:55:22 +0000

    adjust gold_table processing

--- src/03_gold_table.py
+++ src/03_gold_table.py
@@ -6,6 +6,11 @@ from dlt_utils import create_lookup_df, load_yaml_config
 
 # COMMAND ----------
 
+spark.read.format("csv").option("header", "true").load("/FileStore/customer_segmentation/train/").show()
+
+
+# COMMAND ----------
+
 config_path = spark.conf.get("gold_table_config_path")
 gold_table_config  = load_yaml_config(config_path)
 
@@ -59,60 +64,149 @@ def gold_customer_ml_target():
     
     return gold_df_target
 
-@dlt.table
-def debug_gold_customer_features():
-    silver_df = dlt.read("silver_training_customer_data")
-    gold_df = silver_df.drop("id", "segmentation", "inserted_at")
-    gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
-    
-    # Debugging by outputting the intermediate gold_df
-    return gold_df
-
-# # Gold Table: Business-ready or aggregated data
 # @dlt.table
-# def gold_customer_features():
+# def debug_gold_customer_features():
 #     silver_df = dlt.read("silver_training_customer_data")
-#     gold_df = silver_df.drop("id", "segmentation","inserted_at")
-#     # custom imputation for missing values of profession and ever_married
+#     gold_df = silver_df.drop("id", "segmentation", "inserted_at")
 #     gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
-
     
+#     # Debugging by outputting the intermediate gold_df
+#     return gold_df
 
-    
-#     # Custom encoding for Ever_Married, Graduated, Gender, and Spending_Score
-#     for mapping_entry in custom_mappings:
-#         mapping = mapping_entry['mapping']
-#         columns = mapping_entry['columns']
-        
-#         processed_mapping = {str(k) if isinstance(k, bool) else k: str(v) for k, v in mapping.items()}
-        
-#         for column in columns:
-#             gold_df = gold_df.withColumn(
-#                 column, 
-#                 when(col(column).isNotNull() & col(column).isin(processed_mapping.keys()), 
-#                     create_map([(lit(k), lit(v)) for k, v in processed_mapping.items()])[col(column)]
-#                 ).otherwise(lit(None))
-#             )
-    # # Mode Imputation for all categorical columns
-    # for column in categorical_columns_custom:
-    #     mode_value_row = gold_df.groupBy(column).count().orderBy(desc("count")).first()
-    #     if mode_value_row is not None:
-    #         mode_value = mode_value_row[0]
-    #         spark.log(f"Mode value for column {column}: {mode_value} (Type: {type(mode_value)})")
-    #         mode_value = str(mode_value) if isinstance(mode_value, bool) else mode_value
-    #         gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(mode_value)).otherwise(col(column)))
-    #     else:
-    #         spark.log(f"Could not calculate mode for column {column} due to lack of data")
+# Gold Table: Business-ready or aggregated data
+@dlt.table
+def gold_customer_features():
+    silver_df = dlt.read("silver_training_customer_data")
+    gold_df = silver_df.drop("id", "segmentation","inserted_at")
+    # custom imputation for missing values of profession and ever_married
+    gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
 
-    # # Median Imputation for all numerical columns
-    # for column in numerical_columns:
-    #     median_value = gold_df.approxQuantile(column, [0.5], 0.01)[0]
-    #     if median_value is not None:
-    #         spark.log(f"Median value for column {column}: {median_value} (Type: {type(median_value)})")
-    #         median_value = float(median_value)
-    #         gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(median_value)).otherwise(col(column)))
-    #     else:
-    #         spark.log(f"Could not calculate median for column {column} due to lack of data")
+    for mapping_entry in custom_mappings:
+        mapping = mapping_entry['mapping']
+        columns = mapping_entry['columns']
+
+        # Apply mapping for each column in the list
+        for column in columns:
+            # Apply the mapping using when() and lit(), chaining the conditions for each mapping key
+            col_expr = None
+            for key, value in mapping.items():
+                # Add a new when() clause for each mapping
+                if col_expr is None:
+                    col_expr = when(col(column) == key, lit(value))
+                else:
+                    col_expr = col_expr.when(col(column) == key, lit(value))
+
+            # Final fallback if no mapping is found (set to None or leave the column as is)
+            col_expr = col_expr.otherwise(lit(None))
+
+            # Update the DataFrame with the transformed column
+            gold_df = gold_df.withColumn(column, col_expr)
+
+    # Mode Imputation for all categorical columns
+    for column in categorical_columns_custom:
+        mode_value_row = gold_df.groupBy(column).count().orderBy(desc("count")).first()
+        mode_value = mode_value_row[0]
+        mode_value = str(mode_value) if isinstance(mode_value, bool) else mode_value
+        gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(mode_value)).otherwise(col(column)))
+
+
+
+    # Median Imputation for all numerical columns
+    for column in numerical_columns:
+        median_value = gold_df.approxQuantile(column, [0.5], 0.01)[0]
+        median_value = float(median_value)
+        gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(median_value)).otherwise(col(column)))
 
 
     return gold_df
+
+# COMMAND ----------
+
+# ##DEBUG
+# config_path = "../resources/gold_table_config.yml"
+# gold_table_config  = load_yaml_config(config_path)
+# # Define categorical columns for mode imputation
+# # Extracting the column lists
+# categorical_columns_custom = gold_table_config['categorical_columns']['custom']
+# categorical_columns_onehot = gold_table_config['categorical_columns']['onehot']
+# categorical_columns_ordinal = gold_table_config['categorical_columns']['ordinal']
+
+# # Extracting numerical columns
+# numerical_columns = gold_table_config['numerical_columns']
+
+# # Extracting custom mappings
+# custom_mappings = gold_table_config['custom_mappings']
+# custom_mapping_target = gold_table_config['custom_mapping_target']
+
+
+# bronze_df = spark.read.table("customer_segmentation.train_raw")
+# # Standardize column names (lowercase, underscores instead of spaces)
+# df = bronze_df.toDF(*[col.lower().replace(' ', '_') for col in bronze_df.columns])
+
+# # Convert Age and Family Size to integers, Work Experience to float
+# silver_df = (df.withColumn("age", col("age").cast("int"))
+#         .withColumn("family_size", col("family_size").cast("int"))
+#         .withColumn("work_experience", col("work_experience").cast("float"))
+#         )
+
+# gold_df = silver_df.drop("id", "segmentation","inserted_at")
+# # custom imputation for missing values of profession and ever_married
+# gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
+
+
+
+
+
+# display(gold_df)
+
+
+# COMMAND ----------
+
+custom_mappings
+
+# COMMAND ----------
+
+# from pyspark.sql.functions import col, lit, create_map, when
+
+# # Assuming custom_mappings and gold_df are defined
+#     # Loop through each mapping and apply to respective columns
+
+# # Loop through each mapping and apply to respective columns
+# for mapping_entry in custom_mappings:
+#     mapping = mapping_entry['mapping']
+#     columns = mapping_entry['columns']
+
+#     # Apply mapping for each column in the list
+#     for column in columns:
+#         # Apply the mapping using when() and lit(), chaining the conditions for each mapping key
+#         col_expr = None
+#         for key, value in mapping.items():
+#             # Add a new when() clause for each mapping
+#             if col_expr is None:
+#                 col_expr = when(col(column) == key, lit(value))
+#             else:
+#                 col_expr = col_expr.when(col(column) == key, lit(value))
+
+#         # Final fallback if no mapping is found (set to None or leave the column as is)
+#         col_expr = col_expr.otherwise(lit(None))
+
+#         # Update the DataFrame with the transformed column
+#         gold_df = gold_df.withColumn(column, col_expr)
+
+# COMMAND ----------
+
+# # Mode Imputation for all categorical columns
+# for column in categorical_columns_custom:
+#     mode_value_row = gold_df.groupBy(column).count().orderBy(desc("count")).first()
+#     mode_value = mode_value_row[0]
+#     mode_value = str(mode_value) if isinstance(mode_value, bool) else mode_value
+#     gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(mode_value)).otherwise(col(column)))
+
+
+
+# # Median Imputation for all numerical columns
+# for column in numerical_columns:
+#     median_value = gold_df.approxQuantile(column, [0.5], 0.01)[0]
+#     median_value = float(median_value)
+#     gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(median_value)).otherwise(col(column)))
+
