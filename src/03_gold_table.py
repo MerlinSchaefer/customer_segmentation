624856c9761117e521a7c10104b96a29495b6909
MerlinSchaefer <MerlinSchaefer>
2024-10-22 12:40:56 +0000

    prepare integration into dlt pipeline

--- src/03_gold_table.py
+++ src/03_gold_table.py
@@ -2,6 +2,7 @@
 import dlt
 from pyspark.sql.functions import when, col, lit, desc
 from pyspark.sql.types import IntegerType, FloatType
+from pyspark.ml import PipelineModel
 from databricks.sdk.runtime import *
 from dlt_utils import create_lookup_df, load_yaml_config, get_column_data_type
 
@@ -143,91 +144,40 @@ def gold_customer_features():
 
 # COMMAND ----------
 
-# ##DEBUG
-# config_path = "../resources/gold_table_config.yml"
-# gold_table_config  = load_yaml_config(config_path)
-# # Define categorical columns for mode imputation
-# # Extracting the column lists
-# categorical_columns_custom = gold_table_config['categorical_columns']['custom']
-# categorical_columns_onehot = gold_table_config['categorical_columns']['onehot']
-# categorical_columns_ordinal = gold_table_config['categorical_columns']['ordinal']
-
-# # Extracting numerical columns
-# numerical_columns = gold_table_config['numerical_columns']
-
-# # Extracting custom mappings
-# custom_mappings = gold_table_config['custom_mappings']
-# custom_mapping_target = gold_table_config['custom_mapping_target']
-
-
-# bronze_df = spark.read.table("customer_segmentation.train_raw")
-# # Standardize column names (lowercase, underscores instead of spaces)
-# df = bronze_df.toDF(*[col.lower().replace(' ', '_') for col in bronze_df.columns])
-
-# # Convert Age and Family Size to integers, Work Experience to float
-# silver_df = (df.withColumn("age", col("age").cast("int"))
-#         .withColumn("family_size", col("family_size").cast("int"))
-#         .withColumn("work_experience", col("work_experience").cast("float"))
-#         )
-
-# gold_df = silver_df.drop("id", "segmentation","inserted_at")
-# # custom imputation for missing values of profession and ever_married
-# gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
-
-
-
-
-
-# display(gold_df)
-
+  # TODO: MOVE PATH TO CONFIG
+  pipeline_path = "dbfs:/FileStore/customer_segmentation/pipelines/preprocessing" # spark.conf.get("pipeline_path")
 
 # COMMAND ----------
 
-custom_mappings
+@dlt.table
+def gold_scaled_features():
+    silver_df = dlt.read("silver_training_customer_data")
+    gold_df = silver_df.drop("id", "segmentation","inserted_at")
+    # custom imputation for missing values of profession and ever_married
+    gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
 
-# COMMAND ----------
+    # Load the pipeline
+  
+    pipeline_model_scaling = PipelineModel.load(f"{pipeline_path}/scaling")
 
-# from pyspark.sql.functions import col, lit, create_map, when
+    gold_df_transformed = pipeline_model_scaling.transform(gold_df)
 
-# # Assuming custom_mappings and gold_df are defined
-#     # Loop through each mapping and apply to respective columns
+    # TODO: write directly to FeatureStore
+    return gold_df_transformed
 
-# # Loop through each mapping and apply to respective columns
-# for mapping_entry in custom_mappings:
-#     mapping = mapping_entry['mapping']
-#     columns = mapping_entry['columns']
+@dlt.table
+def gold_no_scaled_features():
+    silver_df = dlt.read("silver_training_customer_data")
+    gold_df = silver_df.drop("id", "segmentation","inserted_at")
+    # custom imputation for missing values of profession and ever_married
+    gold_df = gold_df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
 
-#     # Apply mapping for each column in the list
-#     for column in columns:
-#         # Apply the mapping using when() and lit(), chaining the conditions for each mapping key
-#         col_expr = None
-#         for key, value in mapping.items():
-#             # Add a new when() clause for each mapping
-#             if col_expr is None:
-#                 col_expr = when(col(column) == key, lit(value))
-#             else:
-#                 col_expr = col_expr.when(col(column) == key, lit(value))
+    # Load the pipeline
+  
+    pipeline_model_no_scaling = PipelineModel.load(f"{pipeline_path}/no_scaling")
 
-#         # Final fallback if no mapping is found (set to None or leave the column as is)
-#         col_expr = col_expr.otherwise(lit(None))
+    gold_df_transformed = pipeline_model_no_scaling.transform(gold_df)
 
-#         # Update the DataFrame with the transformed column
-#         gold_df = gold_df.withColumn(column, col_expr)
-
-# COMMAND ----------
-
-# # Mode Imputation for all categorical columns
-# for column in categorical_columns_custom:
-#     mode_value_row = gold_df.groupBy(column).count().orderBy(desc("count")).first()
-#     mode_value = mode_value_row[0]
-#     mode_value = str(mode_value) if isinstance(mode_value, bool) else mode_value
-#     gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(mode_value)).otherwise(col(column)))
-
-
-
-# # Median Imputation for all numerical columns
-# for column in numerical_columns:
-#     median_value = gold_df.approxQuantile(column, [0.5], 0.01)[0]
-#     median_value = float(median_value)
-#     gold_df = gold_df.withColumn(column, when(col(column).isNull(), lit(median_value)).otherwise(col(column)))
+    # TODO: write directly to FeatureStore
+    return gold_df_transformed
 
