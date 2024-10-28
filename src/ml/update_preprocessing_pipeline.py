12e1636b0e550125ea29d283651eab25253e3ef0
MerlinSchaefer <MerlinSchaefer>
2024-10-28 09:49:07 +0000

    create job for ml preprocessing pipeline

--- src/ml/update_preprocessing_pipeline.py
+++ src/ml/update_preprocessing_pipeline.py
@@ -1,12 +1,4 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC # Preprocessing Pipeline EXPLORATION
-# MAGIC
-# MAGIC This notebook can be used to explore and experiment with different pipeline options.
-# MAGIC To change the pipelines used in the delta live tables run the job `update_preprocessing_pipelines`
-
-# COMMAND ----------
-
 from pyspark.sql.types import IntegerType, BooleanType, StringType, DoubleType
 from pyspark.sql.functions import col, count, when
 from pyspark.ml import Pipeline
@@ -31,15 +23,7 @@ df = spark.sql("""SELECT
 # COMMAND ----------
 
 # custom preprocessing logic
-df = df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
-
-# COMMAND ----------
-
-df.display()
-
-# COMMAND ----------
-
-train_df = df
+train_df = df.fillna("Other", subset=["profession"]).fillna("No", subset=["ever_married"])
 
 # COMMAND ----------
 
@@ -55,11 +39,6 @@ string_missing_cols = column_info['string_missing_cols']
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC # Imputation and Scaling
-
-# COMMAND ----------
-
 pipeline_builder = FeaturePipelineBuilder(num_cols, string_cols, num_missing_cols)
 pipeline_scaling  = pipeline_builder.create_pipeline(scale_numerical=True)
 pipeline_no_scaling = pipeline_builder.create_pipeline(scale_numerical=False)
@@ -67,40 +46,11 @@ pipeline_no_scaling = pipeline_builder.create_pipeline(scale_numerical=False)
 # COMMAND ----------
 
 pipeline_model_scaling = pipeline_scaling.fit(train_df)
-
-# Transform both training_df  
 train_transformed_scaled_df = pipeline_model_scaling.transform(train_df)
-
-
-
-# COMMAND ----------
-
-train_df.display()
+pipeline_model_scaling.write().overwrite().save("dbfs:/FileStore/customer_segmentation/pipelines/preprocessing/scaling")
 
 # COMMAND ----------
 
 pipeline_model_no_scaling = pipeline_no_scaling.fit(train_df)
-
 train_transformed_no_scaled_df = pipeline_model_no_scaling.transform(train_df)
-
-
-# COMMAND ----------
-
-train_transformed_scaled_df.display()
-
-# COMMAND ----------
-
-train_transformed_no_scaled_df.display()
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC # Save pipeline for use in DLT 
-
-# COMMAND ----------
-
-pipeline_model_no_scaling.write().overwrite().save("dbfs:/FileStore/customer_segmentation/pipelines/preprocessing/experimental/no_scaling")
-
-# COMMAND ----------
-
-pipeline_model_scaling.write().overwrite().save("dbfs:/FileStore/customer_segmentation/pipelines/preprocessing/experimental/scaling")
+pipeline_model_no_scaling.write().overwrite().save("dbfs:/FileStore/customer_segmentation/pipelines/preprocessing/no_scaling")
